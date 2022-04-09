3d687609b278d5439d1ed531101845debd9e0b15
LordStarkLK <2019cs059@stu.ucsc.lk>
2022-04-10 00:06:03 +0530

    moda shaini

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-let nums=document.getElementsByClassName("num");
-let display=document.getElementById('numField');
+class Calculator{
+    constructor(currentNumber,previousNumber){
+        this.currentNumber = currentNumber;
+        this.previousNumber = previousNumber;
+    }
+    clear(){
+        this.currentNumber = '';
+        this.previousNumber = '';
+        this.operation = undefined;
+    }
+    delete(){
 
-//event listners
-nums.addEventListner("click",displayNum(nums));
+    }
+    appendNumber(number){
+        this.currentNumber = number;
+    }
+    chooseOperation(operation){
 
+    }
+    compute(){
 
-//display numbers when clicked
-function displayNum(nums){
-    display.innerText=nums;
+    }
+    update(){
+        console.log(this.currentNumber);
+        this.currentNumber.textContent = this.currentNumber;
+        console.log(this.currentNumber.textContent);
+        
+    }
 }
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-opeartion]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const currentNumber = document.getElementsByClassName("curNumber");
+const previousNumber = document.querySelector("curNumber");
+
+const calculator = new Calculator(currentNumber,previousNumber);
+numberButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText);
+        calculator.update();
+    })
+})
